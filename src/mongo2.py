11c038ad1ee89ba28f59376b9ada470e8457e300
Guillaume BELDILMI <gbeldilmi@pm.me>
2024-03-06 22:39:14 +0100

    Connect to database

--- src/mongo2.py
+++ src/mongo2.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 from pprint import pprint
 
-def connect_mongo2():
+def connect_mongo2(): # Connexion à la base de données
     client = MongoClient("mongo2.iem", port=27017, username="gb232322", password="gb232322", authSource="gb232322", authMechanism="SCRAM-SHA-1")
     db = client.gb232322
     return client, db
--- src/mongo2.sh
+++ src/mongo2.sh
@@ -1,1 +1,3 @@
+#! /usr/bin/env bash
+
 mongo -u gb232322 -p gb232322 -authenticationDatabase gb232322 -host mongo2.iem
